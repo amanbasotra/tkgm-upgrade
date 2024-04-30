abc1e7e8b522135d4c529fa68d13f7ad0b76e09a
amanbasotra <77401393+amanbasotra@users.noreply.github.com>
2024-04-29 18:00:21 -0700

    Update 4_plugininstall.sh
    Updated plugin install script

--- 4_plugininstall.sh
+++ 4_plugininstall.sh
@@ -7,5 +7,8 @@ read -p "Enter plugin group name for tkg: " plugin_version
 tanzu plugin search -n management-cluster --show-details
 tanzu plugin group get vmware-tkg/default:v$plugin_version
 tanzu plugin install --group vmware-tkg/default:v$plugin_version
+read -p "Enter plugin group name for tap: " tap_plugin_version
+tanzu plugin install --group vmware-tap/default:v$tap_plugin_version
+tanzu plugin install --group vmware-tanzucli/essentials
 tkg_plugin_version=`tanzu plugin list`
 echo "$tkg_plugin_version"
